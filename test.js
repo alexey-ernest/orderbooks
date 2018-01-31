6f2140da132b96b38d0747dc53a6ebc2575b4c6b
Alexey Melnikov <alexey.ernest@gmail.com>
2018-01-31 11:08:30 +0300

    Test file and package.json

---
+++ test.js
@@ -0,0 +1,23 @@
+
+const book = require('./orderbooks/hitbtc');
+const beep = require('beeper');
+
+book
+  .on('update', (book, market, ticker) => {
+    // if (ticker === 'PPT') {
+    //   console.log(book[market][ticker].asks[0], book[market][ticker].bids[0]);
+    // }
+    console.log(market, ticker);
+    if (market === 'ETH' && ticker === 'DAT') {
+      console.log(book[market][ticker]);
+    }
+  })
+  .on('error', err => {
+    if (err.code === 'ETIMEDOUT' || err.code === 'ENOTFOUND' || err.code === 'ECONNRESET') {
+      return;
+    }
+
+    console.log(err);
+    beep(1);
+  })
+  .listen();
