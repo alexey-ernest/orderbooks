a7f22c7af722dd3d43a2e403c7386ba057c77ce3
aleksandar.m.nikolov@gmail.com <aleksandar.m.nikolov@gmail.com>
2018-02-14 23:20:10 +0200

    cryptox orderbook script

---
+++ exchanges/cryptox.js
@@ -0,0 +1,205 @@
+/**
+ * Cryptox order book event emitter.
+ */
+
+const EventEmitter = require('events');
+const request = require('request');
+const config = require('config');
+const debug = require('debug')('cointrage:order_book:cryptox');
+
+const API_URL = 'https://cryptox.pl/api';
+const API_MARKETS_URL = 'https://cryptox.pl/js/generated.js';
+const MARKETS_REFRESH_INTERVAL = 30000;
+const BOOKS_REFRSH_INTERVAL = 30000;
+
+const MARKETS = ['ETH', 'BTC', 'USDT', 'USD'];
+
+const parseMarketName = (str) => {
+    const groups = str.split('/');
+    return [groups[1], groups[0]];
+};
+
+const getMarkets = () => new Promise((resolve, reject) => {
+
+    const url = `${API_MARKETS_URL}`;
+    debug(`Getting markets list from url ${url}...`);
+
+    request({
+        uri: url,
+        headers: {
+            'User-Agent': 'request'
+        },
+        json: true,
+        method: 'GET'
+    }, (err, response, body) => {
+        if (err) return reject(err);
+
+        if (response.statusCode !== 200) {
+            // some other error
+            return reject(`Invalid status code received from url ${url}: ${response.statusCode}`);
+        }
+
+        if (!body) {
+            return reject(`Invalid response: ${JSON.stringify(body)}`);
+        }
+
+        // filtering active markets only
+        const markets = {};
+        let counter = 0;
+
+        let exchanges = JSON.parse(body.substring(body.indexOf('var marketSlash=')+16, body.indexOf('var firstCur=')-2));
+
+        for (let mt of exchanges) {
+            let [market, ticker] = parseMarketName(mt);
+            if (MARKETS.indexOf(market) === -1) {
+                continue;
+            }
+
+            if (!markets[market]) {
+                markets[market] = [];
+            }
+
+            counter += 1;
+            markets[market].push(ticker);
+        }
+
+        debug(`Found ${counter} markets`);
+
+        resolve(markets);
+
+    });
+
+});
+
+const getOrderBook = (market, ticker) => new Promise((resolve, reject) => {
+
+    let marketTicker = market + ticker;
+    const url = `${API_URL}/${ticker}${market}/depth.json`;
+    debug(`Getting order book for market ${marketTicker} from url ${url}...`);
+
+    const mapOrder = (o) => {
+        return {
+            rate: o[2],
+            quantity: o[1]
+        };
+    };
+
+    request({
+        uri: url,
+        headers: {
+            'User-Agent': 'request'
+        },
+        json: true,
+        method: 'GET'
+    }, (err, response, body) => {
+        if (err) return reject(err);
+
+        if (response.statusCode !== 200) {
+            return reject(`Invalid status code received from url ${url}: ${response.statusCode}`);
+        }
+
+        if (!body) {
+            return reject(`Invalid response: ${JSON.stringify(body)}`);
+        }
+        else if (!body[`offers${ticker}${market}`]) {
+            return reject(`Invalid response: ${JSON.stringify(body)}`);
+        }
+
+        // formatting response
+        const res = {
+            market: market,
+            ticker: ticker,
+            asks: body[`offers${ticker}${market}`].asks ? body[`offers${ticker}${market}`].asks.map(mapOrder) : [],
+            bids: body[`offers${ticker}${market}`].bids ? body[`offers${ticker}${market}`].bids.map(mapOrder) : []
+        };
+
+        resolve(res);
+    });
+});
+
+class CryptoxOrderBook extends EventEmitter {
+
+    constructor() {
+        super();
+
+        this._book = {};
+    }
+
+    listen() {
+
+        const handleError = (err) => {
+            debug(err);
+            this.emit('error', err);
+        };
+
+        let markets = {};
+        const refreshMarkets = () => {
+            getMarkets()
+                .then((m) => {
+                    markets = m;
+                })
+                .catch(handleError)
+                .then(() => setTimeout(refreshMarkets, MARKETS_REFRESH_INTERVAL));
+        };
+
+        // refreshing markets
+        refreshMarkets();
+
+        const refreshOrderbooks = () => {
+
+            if (!Object.keys(markets).length) {
+                return setTimeout(refreshOrderbooks, BOOKS_REFRSH_INTERVAL);
+            }
+
+            const book = this._book;
+            const self = this;
+
+            let counter = 0;
+
+            for (let m in markets) {
+                for (let t of markets[m]) {
+
+                    (function (market, ticker) {
+                        setTimeout(() => {
+                            getOrderBook(market, ticker)
+                                .then((b) => {
+                                    if (!book[b.market]) {
+                                        book[b.market] = {};
+                                    }
+
+                                    book[b.market][b.ticker] = b;
+                                    self.emit('update', book, b.market, b.ticker);
+                                })
+                                .catch((err) => {
+                                    handleError(err);
+
+                                    if (!book[market]) {
+                                        book[market] = {};
+                                    }
+
+                                    book[market][ticker] = null;
+
+                                    // notifying about market removal
+                                    self.emit('update', book, market, ticker);
+                                });
+                        }, counter * 1000);
+
+                    })(m, t);
+
+                    counter += 1;
+
+                }
+            }
+
+            // schedule next update
+            setTimeout(refreshOrderbooks, BOOKS_REFRSH_INTERVAL);
+        };
+
+        // refreshing order books
+        refreshOrderbooks();
+
+    }
+
+};
+
+module.exports = new CryptoxOrderBook();
