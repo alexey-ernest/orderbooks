7901cffdfa5fce2b481f7a52691bf6e8ad3a2f89
aleksandar.m.nikolov@gmail.com <aleksandar.m.nikolov@gmail.com>
2018-02-16 00:41:58 +0200

    exx orderbook script

---
+++ exchanges/exx.js
@@ -0,0 +1,197 @@
+/**
+ * EXX order book event emitter.
+ *
+ * Each IP can send maximum of 1000 https requests within 1 minute. If exceeds 1000 requests, the system will automatically block the IP for one hour.
+ * After an hour, the IP will be automatically unlocked.
+ *
+ */
+
+const EventEmitter = require('events');
+const request = require('request');
+const config = require('config');
+const debug = require('debug')('cointrage:order_book:exx');
+
+const API_URL = 'https://api.exx.com/data/v1';
+const MARKETS_REFRESH_INTERVAL = 30000;
+const BOOKS_REFRSH_INTERVAL = 30000;
+
+const MARKETS = ['ETH', 'BTC', 'USDT', 'USD'];
+
+const parseMarketName = (str) => {
+    const groups = str.split('_');
+    return [groups[1].toUpperCase(), groups[0].toUpperCase()];
+};
+
+const getMarkets = () => new Promise((resolve, reject) => {
+
+    const url = `${API_URL}/markets`;
+    debug(`Getting markets list from url ${url}...`);
+
+    request({
+        uri: url,
+        json: true,
+        method: 'GET'
+    }, (err, response, body) => {
+        if (err) return reject(err);
+
+        if (response.statusCode !== 200) {
+            // some other error
+            return reject(`Invalid status code received from url ${url}: ${response.statusCode}`);
+        }
+
+        if (!body) {
+            return reject(`Invalid response: ${JSON.stringify(body)}`);
+        }
+
+        // filtering active markets only
+        const markets = {};
+        let counter = 0;
+
+        for (let mt in body) {
+            let [market, ticker] = parseMarketName(mt);
+            if (MARKETS.indexOf(market) === -1) {
+                continue;
+            }
+
+            if (!markets[market]) {
+                markets[market] = [];
+            }
+
+            counter += 1;
+            markets[market].push(ticker);
+        }
+
+        debug(`Found ${counter} markets`);
+
+        resolve(markets);
+
+    });
+
+});
+
+const getOrderBook = (market, ticker) => new Promise((resolve, reject) => {
+
+    let marketTicker = ticker + market;
+    const url = `${API_URL}/depth?currency=${ticker.toLowerCase()}_${market.toLowerCase()}`;
+    debug(`Getting order book for market ${marketTicker} from url ${url}...`);
+
+    const mapOrder = (o) => {
+        return {
+            rate: Number(o[0]),
+            quantity: Number(o[1])
+        };
+    };
+
+    request({
+        uri: url,
+        json: true,
+        method: 'GET'
+    }, (err, response, body) => {
+        if (err) return reject(err);
+
+        if (response.statusCode !== 200) {
+            return reject(`Invalid status code received from url ${url}: ${response.statusCode}`);
+        }
+
+        if (!body) {
+            return reject(`Invalid response: ${JSON.stringify(body)}`);
+        }
+
+        // formatting response
+        const res = {
+            market: market,
+            ticker: ticker,
+            asks: body.asks ? body.asks.map(mapOrder) : [],
+            bids: body.bids ? body.bids.map(mapOrder) : []
+        };
+
+        resolve(res);
+    });
+});
+
+class EXXOrderBook extends EventEmitter {
+
+    constructor() {
+        super();
+
+        this._book = {};
+    }
+
+    listen() {
+
+        const handleError = (err) => {
+            debug(err);
+            this.emit('error', err);
+        };
+
+        let markets = {};
+        const refreshMarkets = () => {
+            getMarkets()
+                .then((m) => {
+                    markets = m;
+                })
+                .catch(handleError)
+                .then(() => setTimeout(refreshMarkets, MARKETS_REFRESH_INTERVAL));
+        };
+
+        // refreshing markets
+        refreshMarkets();
+
+        const refreshOrderbooks = () => {
+
+            if (!Object.keys(markets).length) {
+                return setTimeout(refreshOrderbooks, BOOKS_REFRSH_INTERVAL);
+            }
+
+            const book = this._book;
+            const self = this;
+
+            let counter = 0;
+
+            for (let m in markets) {
+                for (let t of markets[m]) {
+
+                    (function (market, ticker) {
+                        setTimeout(() => {
+                            getOrderBook(market, ticker)
+                                .then((b) => {
+                                    if (!book[b.market]) {
+                                        book[b.market] = {};
+                                    }
+
+                                    book[b.market][b.ticker] = b;
+                                    self.emit('update', book, b.market, b.ticker);
+                                })
+                                .catch((err) => {
+                                    handleError(err);
+
+                                    if (!book[market]) {
+                                        book[market] = {};
+                                    }
+
+                                    book[market][ticker] = null;
+
+                                    // notifying about market removal
+                                    self.emit('update', book, market, ticker);
+                                });
+                        }, counter * 500);
+
+                    })(m, t);
+
+                    counter += 1;
+
+                }
+            }
+
+            // schedule next update
+            setTimeout(refreshOrderbooks, BOOKS_REFRSH_INTERVAL);
+        };
+
+        // refreshing order books
+        refreshOrderbooks();
+
+    }
+
+};
+
+module.exports = new EXXOrderBook();
